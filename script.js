c78872f9874b320e16ef1f244460bd5f19851b9b
Matheus <matheusrcrv07@gmail.com>
2022-06-28 16:01:57 -0300

    Exercicio 01

---
+++ script.js
@@ -0,0 +1,6 @@
+function exercicioUm(){
+  const numeroUm = parseInt(prompt("Digite o primeiro número"))
+  const numeroDois = parseInt(prompt("Digite o segundo número"))
+  alert(`O resto da divisão de ${numeroUm} por ${numeroDois} é: ${(numeroUm % numeroDois)}`)
+  alert(`O número ${numeroUm} elevado a ${numeroDois} é: ${(numeroUm ** numeroDois)}`)
+}
