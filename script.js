64836a3eeefd3d777e84a0e95f138b9b2b4c804a
Matheus <matheusrcrv07@gmail.com>
2022-06-28 16:02:23 -0300

    Exercicio 02

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function exercicioUm(){
   alert(`O resto da divisão de ${numeroUm} por ${numeroDois} é: ${(numeroUm % numeroDois)}`)
   alert(`O número ${numeroUm} elevado a ${numeroDois} é: ${(numeroUm ** numeroDois)}`)
 }
+
+function exercicioDois(){
+  const numeroUm = parseInt(prompt("Digite o primeiro número"))
+  const numeroDois = parseInt(prompt("Digite o segundo número"))
+  alert(`a subtração é: ${(numeroUm - numeroDois)}, a soma é: ${(numeroUm + numeroDois)}, a multiplicação é: ${(numeroUm * numeroDois)}, a divisão é: ${(numeroUm / numeroDois)}`)
+}
